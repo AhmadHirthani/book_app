47b7ab2bc2cf46930e7bd7cd0372209a182f0398
AhmadHirthani <hertani86@gmail.com>
2020-08-09 14:32:03 +0300

    finishFeature2

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-var express=require('express');
-var app=express();
 
-var cors=require('cors');
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('./public'));
+
+
+var cors = require('cors');
 
 require('dotenv').config();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
-PORT=process.env.PORT||3100;
+app.use(express.urlencoded({ extended: true }));
+PORT = process.env.PORT || 3100;
 
 
-
-app.get('/hello', function(request,response){
-response.status(200).render('./pages/index',{
-wellcomeMessage:'helloWorld'
+app.get('/searches/new', function (request, response) {
+    response.status(200).render('./pages/searches/new');
 });
+
+app.get('/hello', function (request, response) {
+    response.status(200).render('./pages/index', {
+        wellcomeMessage: 'helloWorld'
+    });
 })
-app.listen(PORT,function(){
-console.log(`listening on port ${PORT}`);
+
+app.listen(PORT, function () {
+    console.log(`listening on port ${PORT}`);
 })
