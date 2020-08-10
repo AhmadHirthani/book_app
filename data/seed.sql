15626c750adb1a17b0d8accc8e22dcd945e18062
AhmadHirthani <hertani86@gmail.com>
2020-08-10 14:21:45 +0300

    finishFeature1

---
+++ data/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (author, title, isbn, image_url, description, bookshelf) 
+VALUES ('Ali M. Ansari',
+'Confronting Iran',
+'124',
+'http://books.google.com/books/content?id=gOWrka2NY1AC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'An expert on U.S.-Iranian relations explains the past, present, and possible fut',
+'clture');
+
+INSERT INTO books (author, title, isbn, image_url, description, bookshelf) 
+VALUES ('Ali ibn Sultan ibn Muhammad al-Qari al-Harawi',
+'Sarh al-Sifa. (Kommentar zu dem Werke "Sifa fi huquq al-Mustafa", d.i. "Heilung in den Wahrheiten über den auserwählten Propheten", von Abu-l-Fadl',
+'124',
+'http://books.google.com/books/content?id=ncdLAAAAcAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'An expert on U.S.-Iranian relations explains the past, present, and possible fut'
+,'clture');
+
+
